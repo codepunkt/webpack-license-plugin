2e9fd191d75a7036807675913bd22042e503f2c9
Christoph Werner <christoph@codepunkt.de>
2019-08-12 10:43:27 +0200

    chore: bump version, adjust jest config

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,12 @@ module.exports = {
   restoreMocks: true,
   preset: 'ts-jest',
   testEnvironment: 'node',
-  collectCoverageFrom: ['src/**/*.ts', '!src/**/*.d.ts', '!src/index.ts'],
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/*.d.ts',
+    '!src/index.ts',
+    '!src/defaultOptions.ts',
+  ],
   watchPlugins: [
     require.resolve('jest-watch-typeahead/filename'),
     require.resolve('jest-watch-typeahead/testname'),
