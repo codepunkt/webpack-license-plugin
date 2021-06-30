dce5389e1057a361739069ff4621dd779063789a
Friedrich Wilms <FriedrichW@dspace.de>
2021-06-30 08:31:31 +0200

    test: added unit test for webpack v5

--- test/unit/WebpackChunkIterator.test.ts
+++ test/unit/WebpackChunkIterator.test.ts
@@ -3,19 +3,22 @@ import WebpackChunkModuleIterator from '../../src/WebpackChunkModuleIterator'
 import WebpackModuleFileIterator from '../../src/WebpackModuleFileIterator'
 import webpack = require('webpack')
 
-const MockChunk = jest.fn<webpack.compilation.Chunk, any[]>(i => i)
-const MockModuleIterator = jest.fn<WebpackChunkModuleIterator, any[]>(i => i)
-const MockFileIterator = jest.fn<WebpackModuleFileIterator, any[]>(i => i)
+const MockChunk = jest.fn<webpack.compilation.Chunk, any[]>((i) => i)
+const MockModuleIterator = jest.fn<WebpackChunkModuleIterator, any[]>((i) => i)
+const MockFileIterator = jest.fn<WebpackModuleFileIterator, any[]>((i) => i)
+const MockCompilation = jest.fn<webpack.compilation.Compilation, any[]>((i) => i)
 
 describe('WebpackChunkIterator', () => {
   describe('iterateChunks', () => {
     test('returns a list of all file paths', () => {
+      const compilation = new MockCompilation()
+
       const instance = new WebpackChunkIterator(
-        new MockModuleIterator({ iterateModules: (c, cb) => cb(c.name) }),
+        new MockModuleIterator({ iterateModules: (co, c, cb) => cb(c.name) }),
         new MockFileIterator({ iterateFiles: (f, cb) => cb(`/path/to/${f}`) })
       )
 
-      const result = instance.iterateChunks([
+      const result = instance.iterateChunks(compilation, [
         new MockChunk({ name: 'a' }),
         new MockChunk({ name: 'b' }),
       ])
