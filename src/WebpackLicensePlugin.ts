c08c2c8cf91d3ecf8a970235e838f55a68e53df4
Tobias Trumm <tobias@tobiastrumm.de>
2021-10-04 11:53:46 +0200

    fix: conflict error (#619)
    * fix: conflict error
    * Use Set to avoid unnecessary duplicated filenames
    * Raise error instead of silently ignoring licenses

--- src/WebpackLicensePlugin.ts
+++ src/WebpackLicensePlugin.ts
@@ -13,6 +13,11 @@ import WebpackFileSystem from './WebpackFileSystem'
 
 const pluginName = 'WebpackLicensePlugin'
 
+interface ObservedCompiler {
+  name: string;
+  isChild: boolean;
+}
+
 /**
  * @todo "emit" vs "compilation" & "optimizeChunkAssets" hooks
  * @todo add banner to chunks? boolean option + banner formatter?
@@ -23,6 +28,10 @@ const pluginName = 'WebpackLicensePlugin'
  * @todo preferred license types on ambiguity (licenses array or spdx expression)
  */
 export default class WebpackLicensePlugin implements IWebpackPlugin {
+  private readonly filenames = new Set<string>()
+  private createdFiles = false
+  private observedCompilers: ObservedCompiler[] = []
+
   constructor(private pluginOptions: Partial<IPluginOptions> = {}) {}
 
   public apply(compiler: webpack.Compiler) {
@@ -31,12 +40,26 @@ export default class WebpackLicensePlugin implements IWebpackPlugin {
         'webpack-license-plugin',
         this.handleCompilation.bind(this, compiler)
       )
+      compiler.hooks.watchRun.tapAsync(
+        'webpack-license-plugin',
+        this.handleWatchRun.bind(this)
+      )
     } else if (typeof compiler.plugin !== 'undefined') {
       compiler.plugin(
         'compilation',
         this.handleCompilation.bind(this, compiler)
       )
+      compiler.plugin(
+        'watchRun',
+        this.handleWatchRun.bind(this)
+      )
     }
+  }
+
+  public async handleWatchRun(_: unknown, callback: () => void) {
+    this.createdFiles = false
+    this.observedCompilers = []
+    callback()
   }
 
   public handleCompilation(
@@ -62,6 +85,23 @@ export default class WebpackLicensePlugin implements IWebpackPlugin {
     chunks: webpack.compilation.Chunk[],
     callback: () => void
   ) {
+    this.observedCompilers.push({
+      name: compilation.compiler.name,
+      isChild: compilation.compiler.isChild()
+    })
+
+    if (this.createdFiles) {
+      const observedCompilersMessage = this.observedCompilers.map(({ name, isChild }) => `compiler: ${name}, isChild: ${isChild}`).join('\n');
+      const errorMessage = `${pluginName}: Found licenses after license files were already created.\nIf you see this message, you ran into an edge case we thought would not happen. Please open an isssue at https://github.com/codepunkt/webpack-license-plugin/issues with details of your webpack configuration so we can invastigate it further.\n${observedCompilersMessage}`
+      compilation.errors.push(errorMessage)
+      callback()
+      return
+    }
+
+    if (!compilation.compiler.isChild()) {
+      this.createdFiles = true
+    }
+
     const alertAggregator = new WebpackAlertAggregator(compilation)
     const optionsProvider = new OptionsProvider(alertAggregator)
 
@@ -69,7 +109,14 @@ export default class WebpackLicensePlugin implements IWebpackPlugin {
     alertAggregator.flushAlerts(pluginName)
 
     const chunkIterator = new WebpackChunkIterator()
-    const filenames = chunkIterator.iterateChunks(compilation, chunks)
+    for (const filename of chunkIterator.iterateChunks(compilation, chunks)) {
+      this.filenames.add(filename)
+    }
+
+    if (compilation.compiler.isChild()) {
+      callback()
+      return
+    }
 
     const fileSystem = new WebpackFileSystem(compiler.inputFileSystem)
     const packageJsonReader = new PackageJsonReader(fileSystem)
@@ -88,11 +135,9 @@ export default class WebpackLicensePlugin implements IWebpackPlugin {
       )
     )
 
-    await licenseFileWriter.writeLicenseFiles(filenames, options)
+    await licenseFileWriter.writeLicenseFiles([...this.filenames], options)
     alertAggregator.flushAlerts(pluginName)
 
-    if (callback) {
-      callback()
-    }
+    callback()
   }
 }
